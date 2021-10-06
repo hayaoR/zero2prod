908a1d5e6845bba2340ae85a5a25aa535980c56c
Koki Honda <kokihonda@am.ics.keio.ac.jp>
2021-10-07 08:08:38 +0900

    データベースから値を取得するテストを失敗させた

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,6 @@
 use std::net::TcpListener;
+use sqlx::{PgConnection, Connection};
+use zero2prod::configuration::get_configuration;
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -27,8 +29,13 @@ async fn health_check_works() {
 #[actix_rt::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect postgres.");
     let client = reqwest::Client::new();
-    let body = "name=le%20guin&email=ursul_le_guin%40gmail.com";
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
 
     let response = client
         .post(&format!("{}/subscriptions", &app_address))
@@ -39,6 +46,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
         .expect("Failed to execute request.");
 
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions", )
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
+    
+        assert_eq!(saved.email, "ursula_le_guin%40gmail.com");
+        assert_eq!(saved.name, "le guin");
 }
 
 #[actix_rt::test]
